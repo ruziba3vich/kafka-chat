8f80fa812bf7412fe54bb70bc2b4aab9a5066154
ruziba3vich <rruzibayevich@gmail.com>
2025-02-09 23:43:03 +0500

    application creatd

---
+++ src/test/java/com/prodonik/chat_with_kafka/KafkaChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prodonik.chat_with_kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
